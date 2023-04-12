79c427dce690ebc919fc93749eeec69bd86a4da4
Spolpinka <a421243266@gmail.com>
2023-04-12 05:24:59 +0300

    создаем модель

--- src/main/java/com/fssp/analyses/model/Enforcement.java
+++ src/main/java/com/fssp/analyses/model/Enforcement.java
@@ -5,38 +5,101 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement(name = "root") // замените "root" на имя корневого элемента в вашем XML файле
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Enforcement {
-    private long ExternalKey;
-    private String Barcode;
-    private DocType DocType;
-    private String DocName;
-    private LocalDate DocDate;
-    private String DeloNum;
-    private String AuthorName;
-    private String AuthorAddress;
-    private int AuthorPhone;
-    private int AuthorFax;
-    private String AuthorEmail;
-    private int AuthorOkogu;
-    private String AuthorShortName;
-    private String MinistryName;
-    private int AuthorOkpo;
-    private int AuthorDivisionCode;
-    private String AuthorSignerPost;
-    private String AuthorSignerPostCode;
-    private String AuthorSignerName;
-    private String AuthorExecutorName;
-    private int AuthorExecutorPhone;
-    private int QueryKey;
-    private int QueryNumber;
-    private LocalDate QueryDate;
-    private int PageNumber;
-    private int PageCount;
-    private List<Receiver> SendList;
+
+
+        @XmlElement(name = "ExternalKey")
+        private String externalKey;
+
+        @XmlElement(name = "Barcode")
+        private String barcode;
+
+        @XmlElement(name = "DocType")
+        private DocType docType;
+
+        @XmlElement(name = "DocName")
+        private String docName;
+
+        @XmlElement(name = "DocDate")
+        private String docDate;
+
+        @XmlElement(name = "DeloNum")
+        private String deloNum;
+
+        @XmlElement(name = "AuthorName")
+        private String authorName;
+
+        @XmlElement(name = "AuthorAddress")
+        private String authorAddress;
+
+        @XmlElement(name = "AuthorPhone")
+        private String authorPhone;
+
+        @XmlElement(name = "AuthorFax")
+        private String authorFax;
+
+        @XmlElement(name = "AuthorEmail")
+        private String authorEmail;
+
+        @XmlElement(name = "AuthorOkogu")
+        private String authorOkogu;
+
+        @XmlElement(name = "AuthorShortName")
+        private String authorShortName;
+
+        @XmlElement(name = "MinistryName")
+        private String ministryName;
+
+        @XmlElement(name = "AuthorOkpo")
+        private String authorOkpo;
+
+        @XmlElement(name = "AuthorDivisionCode")
+        private String authorDivisionCode;
+
+        @XmlElement(name = "AuthorSignerPost")
+        private String authorSignerPost;
+
+        @XmlElement(name = "AuthorSignerPostCode")
+        private String authorSignerPostCode;
+
+        @XmlElement(name = "AuthorSignerName")
+        private String authorSignerName;
+
+        @XmlElement(name = "AuthorExecutorName")
+        private String authorExecutorName;
+
+        @XmlElement(name = "AuthorExecutorPhone")
+        private String authorExecutorPhone;
+
+        @XmlElement(name = "QueryKey")
+        private String queryKey;
+
+        @XmlElement(name = "QueryNumber")
+        private String queryNumber;
+
+        @XmlElement(name = "QueryDate")
+        private LocalDate queryDate;
+
+        @XmlElement(name = "PageNumber")
+        private int PageNumber;
+
+        @XmlElement(name = "PageCount")
+        private int pageCount;
+
+        @XmlElement(name = "SendList")
+        private List<Receiver> SendList;
+
+
 }
