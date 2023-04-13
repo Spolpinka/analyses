36af9443b9c86664894f816fff78944292ffa054
Spolpinka <a421243266@gmail.com>
2023-04-13 04:15:32 +0300

    создаем модель

--- src/main/java/com/fssp/analyses/model/Receiver.java
+++ src/main/java/com/fssp/analyses/model/Receiver.java
@@ -4,11 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement(name = "root") // замените "root" на имя корневого элемента в вашем XML файле ТОЧНО СЮДА?
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Receiver {
-    private String Receiver;
-    private String ReceiverAddrType;
-    private String ReceiverAddr;
+
+    @XmlElement(name = "Receiver")
+    private String receiver;
+
+    @XmlElement(name = "ReceiverAddrType")
+    private String receiverAddrType;
+
+    @XmlElement(name = "ReceiverAddr")
+    private String receiverAddr;
 }
